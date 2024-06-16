2b23ddd2bf650ffa8c7a1c93754e6ae8eba9b77c
Sendi <sendiaditya@gmail.com>
2024-06-16 18:05:01 +0700

    testnav2

--- app/src/main/java/com/capstone/skinifier/view/main/MainActivity.kt
+++ app/src/main/java/com/capstone/skinifier/view/main/MainActivity.kt
@@ -2,21 +2,21 @@ package com.capstone.skinifier.view.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.capstone.skinifier.R
 import com.capstone.skinifier.databinding.ActivityMainBinding
 import com.capstone.skinifier.view.login.LoginActivity
-import com.capstone.skinifier.view.register.SignupActivity
 import com.capstone.skinifier.view.viewModelFactory.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
     private val mainViewModel by viewModels<MainViewModel> {
         ViewModelFactory.getInstance(this)
     }
@@ -24,13 +24,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         var keepSplashScreen = true
         installSplashScreen().setKeepOnScreenCondition { keepSplashScreen }
 
         setContentView(binding.root)
 
-        //check session
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        // Session check
         mainViewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
                 startActivity(Intent(this, LoginActivity::class.java))
@@ -38,25 +50,5 @@ class MainActivity : AppCompatActivity() {
             }
             keepSplashScreen = false
         }
-
-        setupAction()
     }
-
-    private fun setupAction() {
-        binding.logoutButton.setOnClickListener {
-            val confirmation = AlertDialog.Builder(this)
-                .setTitle(getString(R.string.logout_confirmation))
-                .setMessage(getString(R.string.are_you_sure_you_want_to_logout))
-                .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                    mainViewModel.logout()
-                }
-                .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .create()
-            confirmation.show()
-        }
-    }
-
-
 }
