d95f8635e3a36789e64940b70e303ffc6292a526
Sendi <sendiaditya@gmail.com>
2024-06-17 07:36:09 +0700

    testnav3

---
+++ app/src/main/java/com/capstone/skinifier/view/home/HomeFragment.java
@@ -0,0 +1,6 @@
+package com.capstone.skinifier.view.home;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
--- app/src/main/java/com/capstone/skinifier/view/main/MainActivity.kt
+++ app/src/main/java/com/capstone/skinifier/view/main/MainActivity.kt
@@ -10,11 +10,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.capstone.skinifier.R
 import com.capstone.skinifier.databinding.ActivityMainBinding
 import com.capstone.skinifier.view.login.LoginActivity
 import com.capstone.skinifier.view.register.SignupActivity
 import com.capstone.skinifier.view.viewModelFactory.ViewModelFactory
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
     private val mainViewModel by viewModels<MainViewModel> {
@@ -30,6 +38,20 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
         //check session
         mainViewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
@@ -39,24 +61,24 @@ class MainActivity : AppCompatActivity() {
             keepSplashScreen = false
         }
 
-        setupAction()
+//        setupAction()
     }
 
-    private fun setupAction() {
-        binding.logoutButton.setOnClickListener {
-            val confirmation = AlertDialog.Builder(this)
-                .setTitle(getString(R.string.logout_confirmation))
-                .setMessage(getString(R.string.are_you_sure_you_want_to_logout))
-                .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                    mainViewModel.logout()
-                }
-                .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .create()
-            confirmation.show()
-        }
-    }
+//    private fun setupAction() {
+//        binding.logoutButton.setOnClickListener {
+//            val confirmation = AlertDialog.Builder(this)
+//                .setTitle(getString(R.string.logout_confirmation))
+//                .setMessage(getString(R.string.are_you_sure_you_want_to_logout))
+//                .setPositiveButton(getString(R.string.yes)) { _, _ ->
+//                    mainViewModel.logout()
+//                }
+//                .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+//                    dialog.dismiss()
+//                }
+//                .create()
+//            confirmation.show()
+//        }
+//    }
 
 
 }
